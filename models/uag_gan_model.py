622f89c5a55c2ef8a0123f8f908bb0ca8552ab13
yhlleo <yahui.cvrs@gmail.com>
2019-06-05 16:02:07 +0200

    udpate model

--- models/uag_gan_model.py
+++ models/uag_gan_model.py
@@ -2,7 +2,7 @@ import torch
 import itertools
 from util.image_pool import ImagePool
 from .base_model import BaseModel
-from . import uag_networks
+from . import uag_networks as uag
 
 # TODO: finish the model
 
@@ -16,7 +16,8 @@ class UAGGANModel(BaseModel):
     @staticmethod
     def modify_commandline_options(parser, is_train=True):
         parser.set_defaults(no_dropout=True)
-
+        parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
+        parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
         return parser
 
     def __init__(self, opt):
@@ -27,4 +28,167 @@ class UAGGANModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        pass
+        self.loss_names = []
+        self.visual_names = []
+        if self.isTrain:
+            self.model_names = ['G_att', 'G_img_A', 'G_img_B', 'D']
+        else:  # during test time, only load Gs
+            self.model_names = ['G_att', 'G_img_A', 'G_img_B']
+        
+
+        self.netG_att = uag.define_net_att(opt.input_nc,
+                                           opt.ngf,
+                                           norm=opt.norm,
+                                           init_type=opt.init_type,
+                                           init_gain=opt.init_gain,
+                                           gpu_ids=opt.gpu_ids)
+
+        self.netG_img_A = uag.define_net_img(opt.input_nc,
+                                             opt.output_nc,
+                                             opt.ngf,
+                                             norm=opt.norm,
+                                             init_type=opt.init_type,
+                                             init_gain=opt.init_gain,
+                                             gpu_ids=opt.gpu_ids)
+        
+        self.netG_img_B = uag.define_net_img(opt.input_nc,
+                                             opt.output_nc,
+                                             opt.ngf,
+                                             norm=opt.norm,
+                                             init_type=opt.init_type,
+                                             init_gain=opt.init_gain,
+                                             gpu_ids=opt.gpu_ids)
+
+
+        if self.isTrain:
+            self.netD_A = uag.define_net_dis(opt.input_nc,
+                                             opt.ngf,
+                                             norm=opt.norm,
+                                             init_type=opt.init_type,
+                                             init_gain=opt.init_gain,
+                                             gpu_ids=opt.gpu_ids)
+
+            self.netD_B = uag.define_net_dis(opt.input_nc,
+                                             opt.ngf,
+                                             norm=opt.norm,
+                                             init_type=opt.init_type,
+                                             init_gain=opt.init_gain,
+                                             gpu_ids=opt.gpu_ids)
+
+        if self.isTrain:
+            if opt.lambda_identity > 0.0:  # only works when input and output images have the same number of channels
+                assert(opt.input_nc == opt.output_nc)
+            self.masked_fake_A_pool = ImagePool(opt.pool_size)
+            self.masked_fake_B_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)  # define GAN loss.
+            self.criterionCycle = torch.nn.L1Loss()
+            self.criterionIdt = torch.nn.L1Loss()
+            # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
+            self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_att.parameters(), 
+                self.netG_img_A.parameters(), self.netG_img_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters()), 
+                lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer_D)
+
+
+    def set_input(self, input):
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+
+        Parameters:
+            input (dict): include the data itself and its metadata information.
+
+        The option 'direction' can be used to swap domain A and domain B.
+        """
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def forward(self):
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        # G(A) -> B
+        self.att_A = (self.netG_att(self.real_A)>self.opt.thresh).float()
+        self.fake_B = self.netG_img_A(self.real_A)
+        self.masked_fake_B = self.fake_B*self.att_A + self.real_A*(1-self.att_A)
+        # cycle G(G(A)) -> A
+        self.cycle_att_B = (self.netG_att(self.masked_fake_B)>self.opt.thresh).float()
+        self.cycle_fake_A = self.netG_img_B(self.masked_fake_B)
+        self.cycle_masked_fake_A = self.cycle_fake_A*self.cycle_att_A + self.masked_fake_B*(1-self.cycle_att_A)
+        
+        if self.isTrain:
+            # G(B) -> A
+            self.att_B = (self.netG_att(self.real_B)>self.opt.thresh).float()
+            self.fake_A = self.netG_img_B(self.real_B)
+            self.masked_fake_A = self.fake_A*self.att_B + self.real_B*(1-self.att_B)
+
+            # cycle G(G(B)) -> B
+            self.cycle_att_A = (self.netG_att(self.masked_fake_A)>self.opt.thresh).float()
+            self.cycle_fake_B = self.netG_img_B(self.masked_fake_A)
+            self.cycle_masked_fake_B = self.cycle_fake_B*self.cycle_att_A + self.masked_fake_A*(1-self.cycle_att_A)
+
+    def backward_D_basic(self, netD, real, fake):
+        """Calculate GAN loss for the discriminator
+
+        Parameters:
+            netD (network)      -- the discriminator D
+            real (tensor array) -- real images
+            fake (tensor array) -- images generated by a generator
+
+        Return the discriminator loss.
+        We also call loss_D.backward() to calculate the gradients.
+        """
+        # Real
+        pred_real = netD(real)
+        loss_D_real = self.criterionGAN(pred_real, True)
+        # Fake
+        pred_fake = netD(fake.detach())
+        loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Combined loss and calculate gradients
+        loss_D = (loss_D_real + loss_D_fake) * 0.5
+        loss_D.backward()
+        return loss_D
+    
+    def backward_D_A(self):
+        """Calculate GAN loss for discriminator D_A"""
+        masked_fake_B = self.masked_fake_B_pool.query(self.masked_fake_B)
+        self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, masked_fake_B)
+
+    def backward_D_B(self):
+        """Calculate GAN loss for discriminator D_B"""
+        masked_fake_A = self.masked_fake_A_pool.query(self.masked_fake_A)
+        self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, masked_fake_A)
+
+    def backward_G(self):
+        """Calculate the loss for generators G_A and G_B"""
+        lambda_A = self.opt.lambda_A
+        lambda_B = self.opt.lambda_B
+
+        # GAN loss D_A(G(A))
+        self.loss_G_A = self.criterionGAN(self.netD_A(self.masked_fake_B), True)
+        # GAN loss D_B(G(B))
+        self.loss_G_B = self.criterionGAN(self.netD_B(self.masked_fake_A), True)
+        # Forward cycle loss || G_B(G_A(A)) - A||
+        self.loss_cycle_A = self.criterionCycle(self.cycle_masked_fake_A, self.real_A) * lambda_A
+        # Backward cycle loss || G_A(G_B(B)) - B||
+        self.loss_cycle_B = self.criterionCycle(self.cycle_masked_fake_B, self.real_B) * lambda_B
+        # combined loss and calculate gradients
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B
+        self.loss_G.backward()
+    
+    def optimize_parameters(self):
+        """Calculate losses, gradients, and update network weights; called in every training iteration"""
+        # forward
+        self.forward()      # compute fake images and reconstruction images.
+        # G_A and G_B
+        self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
+        self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
+        self.backward_G()             # calculate gradients for G_A and G_B
+        self.optimizer_G.step()       # update G_A and G_B's weights
+        # D_A and D_B
+        self.set_requires_grad([self.netD_A, self.netD_B], True)
+        self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
+        self.backward_D_A()      # calculate gradients for D_A
+        self.backward_D_B()      # calculate graidents for D_B
+        self.optimizer_D.step()  # update D_A and D_B's weights
--- options/base_options.py
+++ options/base_options.py
@@ -39,6 +39,8 @@ class BaseOptions():
         parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal | xavier | kaiming | orthogonal]')
         parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
         parser.add_argument('--no_dropout', action='store_true', help='no dropout for the generator')
+
+        parser.add_argument('--thresh', type=float, default=0.1, help='threshold of binarizing the attention map')
         
         # dataset parameters
         parser.add_argument('--dataset_mode', type=str, default='unaligned', help='chooses how datasets are loaded.')
